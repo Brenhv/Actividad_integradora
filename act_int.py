b2d98dfe3c5b29349aab3d97866c82ac14278c79
Brenhv <145620267+Brenhv@users.noreply.github.com>
2023-11-22 19:27:09 -0600

    Add files via upload

---
+++ act_int.py
@@ -0,0 +1,53 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import plotly as px
+import plotly.figure_factory as ff
+from bokeh.plotting import figure
+import matplotlib.pyplot as plt
+import time
+import PIL as Image
+
+st.title("Police Incident Reports from 2018 to 2020 in San Francisco")
+
+df = pd.read_csv("Police.csv")
+
+st.markdown('The data shown below belongs to incident reports in the city of San Francisco, from the year 2018 to 2020, with details from each case such as date, day of the week, police district, neighborhood in which it happened, type of incident in category and subcategory, exact location and resolution.')
+
+mapa = pd.DataFrame()
+mapa['Date']= df['Incident Date']
+mapa['Day']= df['Incident Day of Week']
+mapa['Police District']= df['Police District']
+mapa['Neighborhood']= df['Analysis Neighborhood']
+mapa['Incident Category']= df['Incident Category']
+mapa['Incident Subcategory']= df['Incident Subcategory']
+mapa['Resolution']= df['Resolution']
+mapa['lat']= df['Latitude']
+mapa['lon']= df['Longitude']
+mapa = mapa.dropna()
+
+subset_data2 = mapa
+police_district_input = st.sidebar.multiselect(
+    'Police District',
+    mapa.groupby('Police District').count().reset_index()['Police District'].tolist())
+
+if len(police_district_input) > 0:
+    subset_data2 = mapa[mapa['Police District'].isin(police_district_input)]
+
+subset_data1 = subset_data2
+neighborhood_input = st.sidebar.multiselect(
+    'Neighborhood',
+    mapa.groupby('Neighborhood').count().reset_index()['Neighborhood'].tolist())
+
+if len(neighborhood_input) > 0:
+    subset_data1 = subset_data2[subset_data2['Neighborhood'].isin(neighborhood_input)]
+
+subset_data = subset_data1
+incident_input = st.sidebar.multiselect(
+    'Incident category',
+    subset_data1.groupby('Incident Category').count().reset_index()['Incident Category'].tolist())
+
+if len(incident_input) > 0:
+    subset_data = subset_data1[subset_data1['Incident Category'].isin(incident_input)]
+
+subset_data
