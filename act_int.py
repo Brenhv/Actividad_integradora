1a0de9012f6e2ee653621922ab3d5e0783835d45
Brenhv <145620267+Brenhv@users.noreply.github.com>
2023-12-01 21:04:04 -0600

    Update act_int.py

--- act_int.py
+++ act_int.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 #import plotly as px
-from bokeh.plotting import figure
+#from bokeh.plotting import figure
 import matplotlib.pyplot as plt
 import time
 import PIL as Image
